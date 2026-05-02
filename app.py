bbf7e3856150af27156903058ca0d9a69566774e
kristina-x-ux <kristofor677@gmail.com>
2026-05-02 07:17:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет! Введи данные и получи разбор 🔮")
+    bot.send_message(message.chat.id, "Привет! Введи свои данные и я сделаю разбор 🔮")
 
 @bot.message_handler(func=lambda message: True)
 def handle(message):
-    bot.send_message(message.chat.id, "✨ Твой астрологический анализ готов!")
+    bot.send_message(message.chat.id, "Твой астрологический анализ готов ✨")
 
 bot.polling()

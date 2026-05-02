0cee0e026519bd2370d193f8daf7e3ebdbb648be
kristina-x-ux <kristofor677@gmail.com>
2026-05-02 06:39:17 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! Введи данные и получи разбор 🔮")
+
+@bot.message_handler(func=lambda message: True)
+def handle(message):
+    bot.send_message(message.chat.id, "✨ Твой астрологический анализ готов!")
+
+bot.polling()
